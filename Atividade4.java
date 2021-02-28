9f338b23989aedbf3c970936d57ad13d9c83f994
rgz-felipe <77169098+rgz-Felipe@users.noreply.github.com>
2021-02-27 21:23:59 -0300

    Exercicios Faculdade ADS 1 Semana 1/2021
    Exercicios Basicos de Entrada e Saida

---
+++ Atividade4.java
@@ -0,0 +1,23 @@
+package Atividades2502;
+import java.util.Scanner;
+public class Atividade4 {
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String nome;
+        String Endereco;
+        String Cep;
+        String Ctt;
+        
+        nome = " Felipe Ribeiro Gomes";
+        Endereco = " Recanto do Bosque";
+        Cep = " 74474309";
+        Ctt = " 62 981009205";
+		Scanner entrada = new Scanner(System.in);
+       
+		System.out.println("Nome:" + nome + "\nEndereço:" + Endereco + "" );
+		System.out.println("CEP:" + Cep +"\tContato:" + Ctt + "");
+		
+	}
+
+}
---
+++ Atividades1.java
@@ -0,0 +1,16 @@
+package Atividades2502;
+
+public class Atividades1 {
+
+	 //Atividade1
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Este e meu primeiro programa");
+		
+	}
+	
+	
+	
+	
+}
